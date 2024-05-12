3289981b734a02f78922094449d1a03a3f05099b
Serge Maia <50481178+s2erge@users.noreply.github.com>
2024-05-12 22:38:15 +0100

    ADD: New config option for Forge 1.20.1

--- reignitedhud-1.20.1/src/main/java/net/msymbios/reignitedhud/config/ReignitedHudConfig.java
+++ reignitedhud-1.20.1/src/main/java/net/msymbios/reignitedhud/config/ReignitedHudConfig.java
@@ -13,21 +13,42 @@ public class ReignitedHudConfig {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec SPEC;
 
-    public static final ForgeConfigSpec.BooleanValue DURABILITY;
-    public static final ForgeConfigSpec.BooleanValue TARGET;
-    public static final ForgeConfigSpec.BooleanValue TIME;
+    public static final ForgeConfigSpec.BooleanValue ITEM_EQUIPMENT;
+    public static final ForgeConfigSpec.BooleanValue ITEM_HAND;
+
+    public static final ForgeConfigSpec.BooleanValue CLOCK_TIME;
     public static final ForgeConfigSpec.BooleanValue EFFECT;
     public static final ForgeConfigSpec.BooleanValue MOUNT;
+
+    public static final ForgeConfigSpec.BooleanValue PLAYER_SKIN;
+    public static final ForgeConfigSpec.BooleanValue PLAYER_USERNAME;
+    public static final ForgeConfigSpec.BooleanValue PLAYER_HEALTH;
+    public static final ForgeConfigSpec.BooleanValue PLAYER_AIR_SUPPLY;
+    public static final ForgeConfigSpec.BooleanValue PLAYER_ARMOR;
+
+    public static final ForgeConfigSpec.BooleanValue FOOD_LEVEL;
+    public static final ForgeConfigSpec.BooleanValue FOOD_SATURATION;
 
     static {
         BUILDER.push("Reignited HUD");
 
         // DEFINE THE CONFIG
-        DURABILITY = BUILDER.comment("Show/Hide Items Durability").define("durability", true);
-        EFFECT = BUILDER.comment("Show/Hide Current Active Effects").define("effect", true);
-        MOUNT = BUILDER.comment("Show/Hide Mount Information").define("mount", true);
-        TARGET = BUILDER.comment("Show/Hide Entity Information").define("target", true);
-        TIME = BUILDER.comment("Show/Hide Clock").define("time", true);
+
+        PLAYER_SKIN = BUILDER.comment("Show/Hide Player Skins").define("player.skin", false);
+        PLAYER_USERNAME = BUILDER.comment("Show/Hide Player Username").define("player.username", true);
+        PLAYER_HEALTH = BUILDER.comment("Show/Hide Player Health").define("player.health", true);
+        PLAYER_AIR_SUPPLY = BUILDER.comment("Show/Hide Player Air Supply").define("player.air", true);
+        PLAYER_ARMOR = BUILDER.comment("Show/Hide Player Armor").define("player.armor", true);
+
+        FOOD_LEVEL = BUILDER.comment("Show/Hide Food Level").define("food.value", true);
+        FOOD_SATURATION = BUILDER.comment("Show/Hide Food Saturation").define("food.saturation", true);
+
+        ITEM_EQUIPMENT = BUILDER.comment("Show/Hide Items Durability").define("item.equipment", true);
+        ITEM_HAND = BUILDER.comment("Show/Hide Items Durability").define("item.hand", true);
+
+        EFFECT = BUILDER.comment("Show/Hide Current Active Effects").define("misc.effect", true);
+        MOUNT = BUILDER.comment("Show/Hide Mount Information").define("misc.mount", true);
+        CLOCK_TIME = BUILDER.comment("Show/Hide Clock").define("misc.time", true);
 
         BUILDER.pop();
         SPEC = BUILDER.build();
