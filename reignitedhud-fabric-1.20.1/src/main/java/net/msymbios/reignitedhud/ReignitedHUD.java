47be8f71ea230926994c8e0bd2da467cb5dc1ce5
Serge Maia <50481178+s2erge@users.noreply.github.com>
2024-05-18 00:46:32 +0100

    ADD: Fabric 1.20.1 - Version 1.1.0

---
+++ reignitedhud-fabric-1.20.1/src/main/java/net/msymbios/reignitedhud/ReignitedHUD.java
@@ -0,0 +1,19 @@
+package net.msymbios.reignitedhud;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReignitedHUD implements ModInitializer {
+
+	// -- Variables --
+	public static final String MODID = "reignitedhud";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	// -- Methods --
+
+	@Override
+	public void onInitialize() { } // onInitialize ()
+
+} // Class ReignitedHUDEvents
